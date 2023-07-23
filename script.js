317fd3934bc4318208aaa6f6ace4ce70d8f9239f
dennis-22-csc <dennisakpotaire@gmail.com>
2023-07-23 04:20:47 +0100

    separate desktop view from mobile view for article section

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-// Function to check if the user is using a mobile device
-    function isMobileDevice() {
-      return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-    }
-
-    // Redirect to the mobile webpage if the user is on a mobile device
-    if (isMobileDevice()) {
-      // Define the URL of your mobile webpage
-      const mobileWebpageURL = "https://denniscode.tech/mobile.html";
-
-      // Redirect to the mobile webpage
-      window.location.href = mobileWebpageURL;
-      document.body.style.display = "block";
-    } else {
-      document.body.style.display = "block";
-      showMain();
-    }
-
 // Function to show the main container
 function showMain() {
   const nameElement = document.getElementById('name');
@@ -41,60 +23,80 @@ function showMain() {
   }
 
   type();
-	// Set the initial selected card based on currentIndex
-let currentIndex = 0;
-const visibleCards = 1; // Number of visible cards at a time
-
-function updateSelectedCard() {
-  articleCards.forEach((card, index) => {
-    if (index === currentIndex) {
-      card.classList.add('selected');
-    } else {
-      card.classList.remove('selected');
-    }
-  });
+  
+  function isMobileDevice() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  }
+  
+  if (isMobileDevice()) {
+      
+  } else {
+   // Call the function to activate the card desktop view
+   showCardDesktop();
+  }
+       
 }
 
-function navigateCardRight() {
-  currentIndex = Math.min(currentIndex + 1, articleCards.length - visibleCards);
-  const cardWidth = articleCards[currentIndex].offsetWidth;
-  articlesContainer.scrollTo({
-    left: currentIndex * cardWidth,
-    behavior: 'smooth'
-  });
-  updateSelectedCard();
-}
 
-function navigateCardLeft() {
-  currentIndex = Math.max(currentIndex - 1, 0);
-  const cardWidth = articleCards[currentIndex].offsetWidth;
-  articlesContainer.scrollTo({
-    left: currentIndex * cardWidth,
-    behavior: 'smooth'
-  });
-  updateSelectedCard();
-}
+function showCardDesktop() {
+  const articleContainer = document.querySelector('.article-container');
+  articleContainer.style.display = '';
 
-function resizeWindow() {
-  const cardWidth = articleCards[currentIndex].offsetWidth;
-  articlesContainer.scrollTo({
-    left: currentIndex * cardWidth,
-    behavior: 'auto'
-  });
-  updateSelectedCard();
-}
+  const articlesContainer = document.querySelector('.articles-container');
+  
+  let currentIndex = 0;
+  const visibleCards = 1; // Number of visible cards at a time
 
-// Define the articles container variable
-const articlesContainer = document.querySelector('.articles-container');
+  const articleCards = Array.from(document.querySelectorAll('.article-card'));
 
-// Add event listeners to arrow buttons
-const arrowLeft = document.querySelector('.arrow-left');
-const arrowRight = document.querySelector('.arrow-right');
+  function updateSelectedCard() {
+    articleCards.forEach((card, index) => {
+      if (index === currentIndex) {
+        card.classList.add('selected');
+      } else {
+        card.classList.remove('selected');
+      }
+    });
+  }
 
-arrowRight.addEventListener('click', navigateCardRight);
-arrowLeft.addEventListener('click', navigateCardLeft);
-window.addEventListener('resize', resizeWindow);
+  function navigateCardRight() {
+    currentIndex = Math.min(currentIndex + 1, articleCards.length - visibleCards);
+    const cardWidth = articleCards[currentIndex].offsetWidth;
+    articlesContainer.scrollTo({
+      left: currentIndex * cardWidth,
+      behavior: 'smooth'
+    });
+    updateSelectedCard();
+  }
+
+  function navigateCardLeft() {
+    currentIndex = Math.max(currentIndex - 1, 0);
+    const cardWidth = articleCards[currentIndex].offsetWidth;
+    articlesContainer.scrollTo({
+      left: currentIndex * cardWidth,
+      behavior: 'smooth'
+    });
+    updateSelectedCard();
+  }
+
+  function resizeWindow() {
+    const cardWidth = articleCards[currentIndex].offsetWidth;
+    articlesContainer.scrollTo({
+      left: currentIndex * cardWidth,
+      behavior: 'auto'
+    });
+    updateSelectedCard();
+  }
 
   
- }
+  // Add event listeners to arrow buttons
+  const arrowLeft = document.querySelector('.arrow-left');
+  const arrowRight = document.querySelector('.arrow-right');
+
+  arrowRight.addEventListener('click', navigateCardRight);
+  arrowLeft.addEventListener('click', navigateCardLeft);
+  window.addEventListener('resize', resizeWindow);
+}
+
+showMain();
 
